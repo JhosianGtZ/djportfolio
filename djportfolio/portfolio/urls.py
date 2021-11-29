15566a0b52ae27a0c084cb29cb0903a67a32a605
Jhosian <jhosianlpz@gmail.com>
2021-11-28 21:01:07 -0600

    Deploy Configurations

--- djportfolio/portfolio/urls.py
+++ djportfolio/portfolio/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 app_name = "portfolio"
 urlpatterns = [
-    path("", views.home, name="home"),
+    path('', views.home, name="home"),
     path("<int:project_id>", views.detail, name="detail"),
 ]
