9f40a824408c57bae4fc07014f9063ad69b0b763
Jhosian <jhosianlpz@gmail.com>
2021-11-28 20:25:56 -0600

    Deploy Configurations

--- djportfolio/djportfolio/settings.py
+++ djportfolio/djportfolio/settings.py
@@ -122,25 +122,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
-
-MEDIA_ROOT = BASE_DIR / 'static/media/'
-
-MEDIA_URL = "/static/media/"
- 
-
-import os
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'media'),
+    os.path.join(BASE_DIR, 'static'),
 )
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
