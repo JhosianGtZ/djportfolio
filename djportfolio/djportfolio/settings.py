1b12d2d2e8c26c13c7289983b0f7115d441c5704
Jhosian <jhosianlpz@gmail.com>
2021-11-28 20:22:56 -0600

    Deploy Configurations

--- djportfolio/djportfolio/settings.py
+++ djportfolio/djportfolio/settings.py
@@ -122,16 +122,25 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
+
+MEDIA_ROOT = BASE_DIR / 'static/media/'
+
+MEDIA_URL = "/static/media/"
+ 
+
 import os
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'media'),
 )
-
 
 
 # Default primary key field type
