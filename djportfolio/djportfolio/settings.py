fc9fa30e1da936de1a05f009b4dc6e7cee23d9fa
Jhosian <jhosianlpz@gmail.com>
2021-11-28 21:05:01 -0600

    Deploy Configurations

--- djportfolio/djportfolio/settings.py
+++ djportfolio/djportfolio/settings.py
@@ -76,16 +76,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'djportfolio.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-import dj_database_url
-from decouple import config
-
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
