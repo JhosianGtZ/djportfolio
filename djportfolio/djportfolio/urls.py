7c3c5f4ebf0e74d600b1e3d024f695196b1f8b7a
Jhosian <jhosianlpz@gmail.com>
2021-11-29 18:44:34 -0600

    Deploy Configurations

--- djportfolio/djportfolio/urls.py
+++ djportfolio/djportfolio/urls.py
@@ -13,7 +13,9 @@ urlpatterns = [
     path("portfolio/", include('portfolio.urls')),
 ] 
 
-urlpatterns += static(settings.STATIC_URL, settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG: 
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
 
 
 
